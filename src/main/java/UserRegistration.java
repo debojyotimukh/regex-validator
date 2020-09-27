aed05ba9f0a802bd2483d432c42d9cebb5627878
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 22:19:30 +0530

    checkEmail() implemented

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -8,8 +8,19 @@ public class UserRegistration {
     }
 
     public static boolean checkEmail(String email){
-        String[] strings=email.split(".,@");
-        return false;
+        String pattern="^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+        return Pattern.compile(pattern).matcher(email).matches();
     }
 
+    public static boolean checkMobile(String number){
+        String pattern="[0-9]{2}+([ ])+([0-9]{10})";
+        return Pattern.compile(pattern).matcher(number).matches();
+    }
+
+    public static void main(String[] args) {
+        checkEmail("abc.xyz@mnc.com");
+        System.out.println(checkMobile("1A 8961749717"));
+    }
 }
+
+
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -12,7 +12,48 @@ public class UserRegistrationTest {
     }
 
     @Test
-    public void emailTest(){
+    public void emailTestValid(){
+        Assert.assertTrue(UserRegistration.checkEmail("abc@yahoo.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc-100@yahoo.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc.100@yahoo.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc111@abc.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc-100@abc.net"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc.100@abc.com.au"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc@1.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc@gmail.com.com"));
+        Assert.assertTrue(UserRegistration.checkEmail("abc+100@gmail.com"));
 
+    }
+
+    @Test
+    public void emailTestInvalid(){
+        //tld can not start with dot
+        Assert.assertFalse(UserRegistration.checkEmail("abc@.com.my"));
+        // .a is not a valid tld, last tld must contains at least two  characters
+        Assert.assertFalse(UserRegistration.checkEmail("abc123@gmail.a"));
+        //tld can not start with dot
+        Assert.assertFalse(UserRegistration.checkEmail("abc123@.com"));
+        Assert.assertFalse(UserRegistration.checkEmail("abc123@.com.com"));
+
+        //email’s 1st character can not start with dot
+        Assert.assertFalse(UserRegistration.checkEmail(".abc@abc.com"));
+        //email’s is only allow character, digit, underscore and dash
+        Assert.assertFalse(UserRegistration.checkEmail("abc()*@gmail.com"));
+        //email’s tld is only allow character and digit
+        Assert.assertFalse(UserRegistration.checkEmail("abc@%*.com"));
+        //double dots are not allowed
+        Assert.assertFalse(UserRegistration.checkEmail("abc..2002@gmail.com"));
+        //email’s last character can not end with dot
+        Assert.assertFalse(UserRegistration.checkEmail("abc.@gmail.com"));
+        //double @ is not allowed
+        Assert.assertFalse(UserRegistration.checkEmail("abc@abc@gmail.com"));
+        //email’s tld which has two characters can not contains digit
+        Assert.assertFalse(UserRegistration.checkEmail("abc@gmail.com.1a"));
+
+    }
+    @Test
+    public void emailTestMultipleTld(){
+        //cannot have multiple tld
+        Assert.assertFalse(UserRegistration.checkEmail("abc@gmail.com.aa.au"));
     }
 }
